ad0661f23fa893fb20327e6a810dcc0c331cefa1
Divyanshu-yadav-18 <rishabhyadav14065@gmail.com>
2025-05-21 23:40:38 +0530

    maze generation function

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
 import { usePathFinding } from "../hooks/usePathFinding";
+import { useTile } from "../hooks/useTile";
 import { MAZES } from "../utils/constant";
+import { resetGrid } from "../utils/resetGrid";
+import { MazeType } from "../utils/type";
 import { Select } from "./select";
 
 export function Nav(){
-    const {maze} = usePathFinding();
+    const [isDisabled, setIsDisabled] = useState(false)
+    const {maze, setMaze, grid} = usePathFinding();
+    const {startTile, endTile} = useTile();
+
+    const handleGenerateMaze = (maze: MazeType)=>{
+        if(maze==='NONE'){
+            setMaze(maze)
+            resetGrid({grid, startTile, endTile})
+            return;
+        }
+
+        setMaze(maze);
+        setIsDisabled(true)
+    }
     return(
         <div className="flex items-center justify-center min-h-[4.5rem] border-b shadow-gray-600 sm:px-5 px-0">
             <div className="flex items-center lg:justify-between justify-center w-full sm:w-[52rem]">
@@ -16,7 +33,7 @@ export function Nav(){
                         value={maze}
                         options={MAZES}
                         onChange={(e)=>{
-                            //fjslak;k
+                            handleGenerateMaze(e.target.value as MazeType)
                         }}/>
                 </div>
             </div>
