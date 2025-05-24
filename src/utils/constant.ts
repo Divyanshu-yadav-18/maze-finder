ba0dde99a23d9240f422662a4678180e3b780c2a
Divyanshu-yadav-18 <rishabhyadav14065@gmail.com>
2025-05-24 20:08:15 +0530

    recursive maze

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -46,3 +46,5 @@ export const SPEEDS: SpeedSelectType[]= [
     {name: "medium", value:1},
     {name: "fast", value: 0.5}
 ]
+
+export const SLEEP_TIME = 8;
--- src/utils/runMazeAlgorithm.ts
+++ src/utils/runMazeAlgorithm.ts
@@ -1,4 +1,7 @@
 import { binaryTree } from "../lib/algorithms/maze/binaryTree";
+import recursiveDivision from "../lib/algorithms/maze/recursiveDevision";
+import { MAX_COLS, MAX_ROWS, SPEEDS } from "./constant";
+import { constructBorders } from "./constructBorders";
 import { GridType, MazeType, SpeedType, TileType } from "./type";
 
 export const runMazeFunction = async ({
@@ -18,5 +21,22 @@ export const runMazeFunction = async ({
 })=>{
     if( maze == "BINARY_TREE"){
         await binaryTree(grid, startTile,endTile, setIsDisabled,speed)
+    }else if(maze == "RECURSIVE_DIVISION"){
+        const currentSpeed = SPEEDS.find((s)=>s.value === speed)!.value ?? 2
+        await constructBorders(grid, startTile, endTile)
+        await recursiveDivision({
+            grid,
+            startTile,
+            endTile,
+            row:1,
+            col:1,
+            height: Math.floor((MAX_ROWS -1)/2),
+            width: Math.floor((MAX_COLS -1)/2),
+            setIsDisabled,
+            speed,
+        })
+        setTimeout(()=> {
+            setIsDisabled(false);
+        }, 800* currentSpeed);
     }
-}
+};
